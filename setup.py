eb9aff6ba38d9b6eb218e3b440ddc6251fa7a65f
Nicholas Papciak <nicholaspapciak@gmail.com>
2025-05-22 15:25:39 -0700

    python

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name="papaya-debugger",
-    version="0.1.0",
+    version="0.1.1",
     description="Spark debugging and monitoring framework",
     packages=find_packages(),
     include_package_data=True,
-    python_requires=">=3.13",
+    python_requires=">=3.10",
     install_requires=[
         "discord.py>=2.3.2",
         "google-genai>=1.14.0",
